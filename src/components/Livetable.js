e4cad5e6f3efe92566f084b496e1c313aba068d4
matsaina <matsaina@gmail.com>
2023-05-05 09:07:35 +0300

    changes

--- src/components/Livetable.js
+++ src/components/Livetable.js
@@ -1,51 +1,9 @@
-import React, { useEffect, useState } from "react";
-import TableCard from "./TableCard";
+import React from 'react'
 
-function LiveTable({ allData }) {
-  const [liveData, SetLiveData] = useState([]);
-
-  useEffect(() => {
-    SetLiveData(
-      allData.filter((live) => {
-        return (
-          live.fixture.status.short == "2H" ||
-          live.fixture.status.short == "1H" ||
-          live.fixture.status.short == "HF"
-        );
-      })
-    );
-  }, [allData]);
-  
-
+function LiveTable() {
   return (
-    <>
-      <h2>Live Games</h2>
-      <div className="table-responsive">
-        <table className="table table-striped table-sm">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th style={{ textAlign: "center" }}>Home</th>
-              <th>Scores</th>
-              <th style={{ textAlign: "center" }}>Away</th>
-              <th>Time</th>
-              <th>status</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {liveData.map((team) => {
-              return (
-                <>
-                  <TableCard team={team} />
-                </>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-    </>
-  );
+    <div>LiveTable</div>
+  )
 }
 
-export default LiveTable;
+export default LiveTable
